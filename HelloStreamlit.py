13320df417c9f0ffff816297f5c10550c48cfe98
ed <edward.changzk@gmail.com>
2026-03-29 02:08:25 +0800

    Delete HelloStreamlit.py

--- HelloStreamlit.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("My name is Ed")
-st.header("About Me")
-st.text("1. I'm 22 this year")
-st.text("2. I love exercising")
-st.text("3. I love this class")
-
-st.success("Welcome to my first Streamlit App!")
